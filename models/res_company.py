1f582b4702071b854cddd8201174cd724bb2fc6b
Ignacio <ibuioli@gmail.com>
2021-12-14 18:43:09 -0300

    feat: add pagination

--- models/res_company.py
+++ models/res_company.py
@@ -33,9 +33,23 @@ class ResCompany(models.Model):
            'Authorization': 'Bearer ' + self.canvas_token,
         }
 
-        response = requests.get(self.canvas_url + '/api/v1/accounts/self/users', headers=headers)
-        _logger.info(response.text)
-        raise ValidationError('Importar Usuarios')
+        response = requests.get(self.canvas_url + '/api/v1/accounts/self/users', headers=headers, params={'per_page':10})
+
+        if response.status_code == 200:
+            paginated = response.json()
+            while 'next' in response.links:
+               response = requests.get(response.links['next']['url'], headers=headers)
+               paginated.extend(response.json())
+            self.process_user(paginated)
+        else:
+            raise ValidationError('Conexión no establecida o falta de permisos. Contacte a su administrador. ' + response.text)
+
+    def process_user(self, r):
+        for rec in r:
+            _logger.info(rec['name'])
+            partner = self.env['res.partner'].search([('email', '=', rec['email'])], limit='1')
+            if not partner:
+                _logger.info('Sin partner')
 
     canvas_url = fields.Char(string='Canvas URL', help='This is the Canvas URL of site')
     canvas_token = fields.Char(string='Canvas API Token', help='Canvas API Token')
